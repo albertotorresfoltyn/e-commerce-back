2f364a2a51021d690014389b647f583d60c2945e
Pablo Calvano <pcalvano@celltrak.com>
2019-08-31 18:21:19 -0300

    Products & Cateogires ABM

--- controllers/product.js
+++ controllers/product.js
@@ -5,6 +5,28 @@ const fs = require ("fs"); //read file sistems
 const Product = require("../models/product");
 const { errorHandler } = require ("../helpers/dbErrorHandler");
 
+exports.productById =  (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: "Product not found"
+            });
+        }
+            req.product = product 
+            // if found the prod on db we make it available on the req obj w the name of product
+            next();
+    });
+};
+
+
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
+
+
+
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm() // form data will be available from form
     form.keepExtensions = true, //file we get, extension we have
@@ -56,3 +78,76 @@ exports.create = (req, res) => {
 
     })
 };
+
+exports.remove = (req, res) => {
+    let product = req.product;
+    product.remove((err, deletedProduct) =>{
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            deletedProduct,
+            message: "Product deleted successfully"
+        });
+    });
+
+};
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err){
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            });
+        }
+
+        //check all the fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields;
+
+        if (!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error: "All fields are required"
+            });
+        }
+        // UPDATING THE PRODUCTS
+        let product = req.product 
+        product = _.extend(product, fields)
+
+         //1kb = 1000
+        //1mb = 100000
+
+        if(files.photo){ //we send the photo from the client side (postman)
+            //console.log("FILES PHOTO: ", files.photo);
+
+            if (files.photo.size > 1000000 ){
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            }
+
+            product.photo.data = fs.readFileSync(files.photo.path) //as products Schema model
+            product.photo.contentType = files.photo.type
+        }
+
+        product.save((err,result) => {
+            if(err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        })
+
+    })
+}
