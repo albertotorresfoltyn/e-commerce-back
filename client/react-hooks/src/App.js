1eccc2bb4b09224a17334923f6709a9b1880e44c
Pablo Calvano <pcalvano@celltrak.com>
2019-09-03 11:54:28 -0300

    CarpetaClient con ReactApp & ecommerce-front

--- client/react-hooks/src/App.js
+++ client/react-hooks/src/App.js
@@ -3,29 +3,59 @@ import React, { Component, useState, useEffect } from 'react';
 const App = () => {
   //state
   const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react')
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react')
+  const [loading, setLoading] = useState(false);
 
+  
   //fetch news
-
   const fetchNews = () => {
-    fetch("http://hn.algolia.com/api/v1/search?query=react")
+    //set loading true
+    setLoading(true)
+
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
     .then(result => result.json())
     //.then(data => console.log(data)); //ver la data en consola
-    .then(data => setNews(data.hits))
+    .then(data => (setNews(data.hits), setLoading(false)))
     .catch(error => console.log(error));
   };
 
   useEffect(() => {
     fetchNews();
-  });
+  }, [url]);
+
+  const handleChange = e => {
+    setSearchQuery(e.target.value);
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
+
+  const showLoading = () => (loading ? <h2>Loading...</h2> : "");
+
+  const searchForm = () => (
+    <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange}/>
+        <button>Search</button>
+      </form>
+  )
+
+  const showNews = () => (
+     news.map((n, i) => (
+      <p key={i}> {n.title} </p> 
+    ))
+  )
+
   return (
     <div>
       <h2>News</h2>
-      {news.map((n, i) => (
-        <p key={i}> {n.title} </p>
-      ))}
+      {showLoading()}
+      {searchForm()}
+      {showNews()}
     </div>
   );
-
 };
 
 /** 
