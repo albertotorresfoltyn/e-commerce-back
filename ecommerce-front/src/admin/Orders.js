a917321cd2955825149380448ec4523aa79fbc10
Alberto Torres <albertotorresfoltyn@gmail.com>
2019-09-16 00:08:17 -0300

    deschingadas

--- ecommerce-front/src/admin/Orders.js
+++ ecommerce-front/src/admin/Orders.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
-import { isAuth } from "../auth"
+import { authenticate as isAuth} from "../auth/index"
 import { Link } from "react-router-dom"
 import { listOrders } from "./apiAdmin"
 
--- ecommerce-front/src/core/Cart.js
+++ ecommerce-front/src/core/Cart.js
@@ -11,7 +11,7 @@ const Cart = () => {
     useEffect(() => {
         //console.log('using setitems', getCart());
         setItems(getCart())
-    }, [init])
+    }, [items])
 
     const showItems = items => {
         return (
